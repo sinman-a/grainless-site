4d58a44721dd937ba1b826c18d24c766f2059355
sinman-a <almazor.schwab@gmail.com>
2025-05-03 13:28:09 +0300

    footer bg changes

--- Grainless.js
+++ Grainless.js
@@ -2,3 +2,19 @@
 document.querySelector('.btn-primary')?.addEventListener('click', () => {
   alert('Перехід до каталогу скоро буде реалізовано 😉');
 });
+
+ function setLanguage(lang) {
+      document.querySelectorAll('[data-en]').forEach(el => {
+        const translation = el.getAttribute(`data-${lang}`);
+        if (translation) {
+          el.textContent = translation;
+        }
+      });
+      localStorage.setItem('lang', lang);
+      document.documentElement.lang = lang;
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+      const savedLang = localStorage.getItem('lang') || 'uk';
+      setLanguage(savedLang);
+    });
